16e17a4d148faad95366761bb1659acf9e01d63a
Eashwar Sridharan <esridharan1@student.ohlone.edu>
2026-04-25 19:17:50 -0700

    Some framework for quiz testing added

--- src/main/java/Quiz.java
+++ src/main/java/Quiz.java
@@ -2,9 +2,18 @@ import java.util.ArrayList;
 
 public class Quiz {
 
+    public Quiz(String id){
+        //placeholder constructor;
+    }
+
     private ArrayList<Question> questions;
 
-    public ArrayList<Question> getQuestions(){
+    public int getQuestionCount(){
+        return questions.size();
+    }
+
+    //Private because intended for use only within quiz.take()
+    private ArrayList<Question> getQuestions(){
         return questions;
     }
 
@@ -12,7 +21,26 @@ public class Quiz {
      * Take each question in the quiz
      * @return Total score for quiz
      */
-    public float Take(){
-       return 0;
+    public float take(){
+        return 0;
+    }
+
+    //Question class is tied to quiz and thus only accessible within quiz
+    public static class Question {
+    /*Questions can also have their own score instead of all being worth an equal amount, if wanted,
+    in which case implementation would be slightly different*/
+
+        private String type; //Use enum, decides whether a question is multiple answer or single answer
+
+        /**
+         *
+         * @param answers A 0 or 1 for every possible answer to indicate whether it's input as an answer
+         *                (can be changed based on implementation for multiple answers)
+         * @return score for question
+         */
+        public float Answer(boolean[] answers){
+            //Behavior should depend on type enum
+            return 0;
+        }
     }
 }
